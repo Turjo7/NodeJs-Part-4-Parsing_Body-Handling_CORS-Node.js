b43073ebf52ccf52783f81768d825fbf44b19bf1
Nasir Uddin Ahmed <43819252+Turjo7@users.noreply.github.com>
2019-10-04 21:54:57 +0600

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',(req,res,next)=>{
+  res.status(200).json({
+    message: "Orders Were Fetched"
+  });
+});
+
+
+router.post('/',(req,res,next)=>{
+  const order = {
+    productId : req.body.productId,
+    quantity : req.body.quantity
+  };
+  res.status(201).json({
+    message: "Orders Were Created",
+    order : order
+  });
+});
+
+
+
+router.get('/:orderId',(req,res,next)=>{
+  res.status(201).json({
+    message: "Orders Were Created",
+    orderId: req.params.orderId
+  });
+});
+
+
+router.delete('/:orderId',(req,res,next)=>{
+  res.status(201).json({
+    message: "Orders Deleted",
+    orderId: req.params.orderId
+  });
+});
+module.exports = router;
